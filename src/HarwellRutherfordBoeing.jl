ea9170149df8b6d15cbfff92393a4f57851baa35
Dominique Orban <dominique.orban@gmail.com>
2016-12-06 23:07:20 -0500

    write matrices and data in RB format

--- src/HarwellRutherfordBoeing.jl
+++ src/HarwellRutherfordBoeing.jl
@@ -3,9 +3,11 @@ module HarwellRutherfordBoeing
 using Compat
 
 export HarwellBoeingMatrix, RutherfordBoeingData
+export write_rb, write_rb_aux
 
 include("hrb_utils.jl")
 include("harwell_boeing.jl")
 include("rutherford_boeing.jl")
+include("write_rb.jl")
 
 end
